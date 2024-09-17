ee4a686cdfd154bb6071593df465d7cfcff4bbeb
casengchiam <casengchiam@gmail.com>
2024-09-16 22:44:13 -0500

    modifying first file, adding puts line to nthmax

--- first_file.rb
+++ first_file.rb
@@ -21,22 +21,6 @@ def fib(n)
   return array
 end
 
-def isPalindrome(n)
-  #takes in an integer, returns a boolean, true or false
-  n_str = n.to_s
-  n_rev = n_str.reverse
-  return n_str.eql?n_rev
-end
+# modifying first file
 
-def nthmax(n, a)
-  # takes in an integer and an array 
-  # the largest integer is the 0th
-  # returns nil if number DNE
-  # we make assumptions that n is non negative
-  if n > a.length-1 # not enough elements in the array
-    return nil
-  end
-  a.sort! # sort the array
-  a.reverse!
-  return a[n]
-end
+puts nthmax(0,[3,2,1,7])
